136485a2fa84250077b74f2fd9240df3f0e20b7d
Alejandro  Zambrano Lasso <alejandrozambranolasso@MacBook-Pro-de-Alejandro.local>
2023-02-05 09:15:39 -0500

    add re.js

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firstName; // Undefined
+firstName = 'Alejandro';
+console.log(firstName);
+
+var lastName = "David"; // declarar / asignar
+lastName = 'Alejandro'; // reasignar
+console.log(lastName);
+
+var secondName = 'David'; // declaración / asignando
+var secondName = 'Alejandro'; // reasignado
+console.log(secondName);
+
+// let
+let fruit = 'Apple' // declara y asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+// const 
+const animal = 'dog'; // declara y asigna
+// animal = 'cat'; // reasignando
+const animal = 'Snake';
+console.log(animal);
+
+const vehicles = []
+vehicles.push("🚗")
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
