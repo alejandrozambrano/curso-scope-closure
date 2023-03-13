a9638a77ea3b478269aec9e8fe126e090a0c5584
Alejandro  Zambrano Lasso <alejandrozambranolasso@MacBook-Pro-de-Alejandro.local>
2023-03-13 16:39:25 -0500

    add index.js for debug

--- src/hoisting/index.js
+++ src/hoisting/index.js
@@ -1,2 +1,15 @@
-console.log(nameOfDog);
-var nameOfDog = 'Elmo';
+
+// var nameOfDog; // undefined
+
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+
+
+var elmo = 'Elmito'; 
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
